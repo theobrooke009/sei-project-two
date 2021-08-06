bd03f536938bd0218d32f13a881b1b1a1401c22d
Theo Brooke <theobrooke009@gmail.com>
2021-08-06 11:23:05 +0100

    working logic for all states

--- src/components/heros/HeroGame.js
+++ src/components/heros/HeroGame.js
@@ -32,6 +32,8 @@ function getPowerStat() {
 }
 
 
+
+
 function HeroGame() {
   const [heroes, setHeroes] = React.useState(null)
   const [heroRight, setHeroRight] = React.useState(null)
@@ -39,36 +41,87 @@ function HeroGame() {
   const [powerStats, setPowerStats] = React.useState('')
 
   const [playerChoice, setPlayerChoice] = React.useState('')
-  const [powerStatsLeft, setPowerStatsLeft] = React.useState('')
-  const [powerStatsRight, setPowerStatsRight] = React.useState('')
-  
+
+  // comparing each powerstat individually
+  // intelligence win, intelligence loss, intelligence draw
+
+  // render a statement for each win, loss or draw 
+
+    
   const isIntelligenceWin = powerStats === 'intelligence' &&
-  playerChoice === heroLeft && powerStatsLeft.intelligence > powerStatsRight.intelligence ||
-  playerChoice === heroRight && powerStatsRight.intelligence > powerStatsLeft.intelligence
+  playerChoice === heroLeft && heroLeft.powerstats.intelligence > heroRight.powerstats.intelligence ||
+  playerChoice === heroRight && heroRight.powerstats.intelligence > heroLeft.powerstats.intelligence
 
+  const isIntelligenceLose = powerStats === 'intelligence' &&
+  playerChoice === heroLeft && heroLeft.powerstats.intelligence < heroRight.powerstats.intelligence ||
+  playerChoice === heroRight && heroRight.powerstats.intelligence < heroLeft.powerstats.intelligence
 
+  const isIntelligenceDraw = powerStats === 'intelligence' && 
+  playerChoice === heroLeft && heroLeft.powerstats.intelligence === heroRight.powerstats.intelligence || 
+  playerChoice === heroRight && heroRight.powerstats.intelligence === heroLeft.powerstats.intelligence
 
   const isSpeedWin = powerStats === 'speed' &&
-  playerChoice === heroLeft && powerStatsLeft.speed > powerStatsRight.speed ||
-  playerChoice === heroRight && powerStatsRight.speed > powerStatsLeft.speed
-                         
+  playerChoice === heroLeft && heroLeft.powerstats.speed > heroRight.powerstats.speed ||
+  playerChoice === heroRight && heroRight.powerstats.speed > heroLeft.powerstats.speed 
+  
+  const isSpeedLose = powerStats === 'speed' &&
+  playerChoice === heroLeft && heroLeft.powerstats.speed < heroRight.powerstats.speed ||
+  playerChoice === heroRight && heroRight.powerstats.speed < heroLeft.powerstats.speed
+
+  const isSpeedDraw = powerStats === 'speed' && 
+  playerChoice === heroLeft && heroLeft.powerstats.speed === heroRight.powerstats.speed || 
+  playerChoice === heroRight && heroRight.powerstats.speed === heroLeft.powerstats.speed
+                      
   const isCombatWin = powerStats === 'combat' &&
-  playerChoice === heroLeft && powerStatsLeft.combat > powerStatsRight.combat ||
-  playerChoice === heroRight && powerStatsRight.combat > powerStatsLeft.combat
+  playerChoice === heroLeft && heroLeft.powerstats.combat > heroRight.powerstats.combat ||
+  playerChoice === heroRight && heroRight.powerstats.combat > heroLeft.powerstats.combat
+
+  const isCombatLose = powerStats === 'combat' &&
+  playerChoice === heroLeft && heroLeft.powerstats.combat < heroRight.powerstats.combat ||
+  playerChoice === heroRight && heroRight.powerstats.combat < heroLeft.powerstats.combat
+
+  const isCombatDraw = powerStats === 'combat' && 
+  playerChoice === heroLeft && heroLeft.powerstats.combat === heroRight.powerstats.combat || 
+  playerChoice === heroRight && heroRight.powerstats.combat === heroLeft.powerstats.combat
 
   const isDurabilityWin = powerStats === 'durability' &&
-  playerChoice === heroLeft && powerStatsLeft.durability > powerStatsRight.durability ||
-  playerChoice === heroRight && powerStatsRight.durability > powerStatsLeft.durability
+  playerChoice === heroLeft && heroLeft.powerstats.durability > heroRight.powerstats.durability ||
+  playerChoice === heroRight && heroRight.powerstats.durability > heroLeft.powerstats.durability
+
+  const isDurabilityLose = powerStats === 'durability' &&
+  playerChoice === heroLeft && heroLeft.powerstats.durability < heroRight.powerstats.durability ||
+  playerChoice === heroRight && heroRight.powerstats.durability < heroLeft.powerstats.durability
+
+  const isDurabilityDraw = powerStats === 'durability' &&
+  playerChoice === heroLeft && heroLeft.powerstats.durability === heroRight.powerstats.durability ||
+  playerChoice === heroRight && heroRight.powerstats.durability === heroLeft.powerstats.durability
 
   const isPowerWin = powerStats === 'power' &&
-  playerChoice === heroLeft && powerStatsLeft.power > powerStatsRight.power ||
-  playerChoice === heroRight && powerStatsRight.power > powerStatsLeft.power
+  playerChoice === heroLeft && heroLeft.powerstats.power > heroRight.powerstats.power ||
+  playerChoice === heroRight && heroRight.powerstats.power  > heroLeft.powerstats.power 
+
+  const isPowerLose = powerStats === 'power' &&
+  playerChoice === heroLeft && heroLeft.powerstats.power < heroRight.powerstats.power ||
+  playerChoice === heroRight && heroRight.powerstats.power  < heroLeft.powerstats.power 
+
+  const isPowerDraw = powerStats === 'power' &&
+  playerChoice === heroLeft && heroLeft.powerstats.power === heroRight.powerstats.power ||
+  playerChoice === heroRight && heroRight.powerstats.power  === heroLeft.powerstats.power 
 
   const isStrengthWin = powerStats === 'strength' &&
-  playerChoice === heroLeft && powerStatsLeft.strength > powerStatsRight.strength  ||
-  playerChoice === heroRight && powerStatsRight.strength  > powerStatsLeft.strength 
+  playerChoice === heroLeft && heroLeft.powerstats.strength > heroRight.powerstats.strength ||
+  playerChoice === heroRight && heroRight.powerstats.strength  > heroLeft.powerstats.strength
 
+  const isStrengthLose = powerStats === 'strength' &&
+  playerChoice === heroLeft && heroLeft.powerstats.strength < heroRight.powerstats.strength ||
+  playerChoice === heroRight && heroRight.powerstats.strength  < heroLeft.powerstats.strength
 
+  const isStrengthDraw = powerStats === 'strength' &&
+  playerChoice === heroLeft && heroLeft.powerstats.strength === heroRight.powerstats.strength ||
+  playerChoice === heroRight && heroRight.powerstats.strength  === heroLeft.powerstats.strength
+
+  
+  
   React.useEffect(() => {
     const getData = async () => {
       const response = await getAllHeros()
@@ -81,57 +134,63 @@ function HeroGame() {
     }
     getData()
   },[])
-
-  console.log('power stats new', powerStats)
-
-  // if the powerStat string we have set up is the same as the hero.powerstat 
-  // set that as the powerstat to match win states
-
-  console.log(heroes)
-
-
  
-  
-
-
   function handleClick(e) {
     // setting the power stats for each card
-    setPowerStatsLeft(heroLeft.powerstats)
-    setPowerStatsRight(heroRight.powerstats)
     if (e.currentTarget.innerText === heroLeft.name) {
       // setting the player choice
       setPlayerChoice(heroLeft)  
     } else {
       setPlayerChoice(heroRight) 
     }
-
-    const winPowerState = () => {
-      return heroes.filter(hero =>{
-        if (heroLeft.powerstats === powerStats){
-          console.log('match')
-        }
-      })
-    }
-  
-    winPowerState()
-
+    console.log('power stats new', powerStats)
+    console.log('hero left stats', heroLeft.powerstats)
+    console.log('hero right stats', heroRight.powerstats)
   }
 
   return (
     <section>
       <div className="container"> 
         
-        {playerChoice && isIntelligenceWin && <p>You int win</p>}
+        {playerChoice && powerStats === 'intelligence' && isIntelligenceWin && <p>You int win</p>}
+
+        {playerChoice &&  powerStats === 'intelligence' && isIntelligenceLose && <p>You int lose</p>}
+
+        {playerChoice && powerStats === 'intelligence' &&  isIntelligenceDraw && <p>You int draw</p>}
+
+        {playerChoice && powerStats === 'speed' && isSpeedWin && <p>You speed win</p>}
+
+        {playerChoice &&  powerStats === 'speed' && isSpeedLose && <p>You speed lose</p>}
+
+        {playerChoice && powerStats === 'speed' && isSpeedDraw && <p>You speed draw</p>}
+
+        {playerChoice && powerStats === 'combat' && isCombatWin && <p>You combat win</p>}
+
+        {playerChoice &&  powerStats === 'combat' && isCombatLose  && <p>You combat lose</p>}
+
+        {playerChoice && powerStats === 'combat' && isCombatDraw && <p>You combat draw</p>}
+
+        {playerChoice && powerStats === 'durability' && isDurabilityWin && <p>You durability win</p>}
+
+        {playerChoice &&  powerStats === 'durability' && isDurabilityLose  && <p>You durability lose</p>}
+
+        {playerChoice && powerStats === 'durability' && isDurabilityDraw && <p>You durability draw</p>}
+
+        {playerChoice && powerStats === 'power' && isPowerWin && <p>You power win</p>}
+
+        {playerChoice &&  powerStats === 'power' && isPowerLose  && <p>You power lose</p>}
+
+        {playerChoice && powerStats === 'power' && isPowerDraw && <p>You power draw</p>}
+
+        {playerChoice && powerStats === 'strength' && isStrengthWin && <p>You strength win</p>}
+
+        {playerChoice &&  powerStats === 'strength' && isStrengthLose  && <p>You strength lose</p>}
+
+        {playerChoice && powerStats === 'strength' && isStrengthDraw && <p>You strength draw</p>}
         
-        {playerChoice && isSpeedWin && <p>You speed win</p>}
         
-        {playerChoice && isCombatWin && <p>You combat win</p>}
-        
-        {playerChoice && isDurabilityWin && <p>You dur win</p>}
-        
-        {playerChoice && isPowerWin && <p>You power win</p>}
-      
-        {playerChoice && isStrengthWin && <p>You strength win</p>}
+
+     
 
     
         <div onClick={handleClick} className="columns">
